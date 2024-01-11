317d6ddbca100b88d94bca1808a57c82634e9374
Lamaglama39 <lamaglama621@gmail.com>
2024-01-11 21:07:44 +0900

    :recycle: config, split into functions

---
+++ app/modules/config.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# set conig
+st.set_page_config(
+    page_title="LogMystic",
+    page_icon="❓",
+    # layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://github.com/Lamaglama39/log-mystic',
+        'Report a bug': 'https://github.com/Lamaglama39/log-mystic',
+        'About': "Log Search app!"
+    }
+)
